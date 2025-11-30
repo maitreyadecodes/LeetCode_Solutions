51722b38d1277d16ad1ad1b6cef2668fd3d8118f
maitreyadecodes <maitreya1335@gmail.com>
2025-11-30 12:56:15 +0530

    AS

--- Arrays/1590divbyp.cpp
+++ Arrays/1590divbyp.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+using namespace std;
+
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
